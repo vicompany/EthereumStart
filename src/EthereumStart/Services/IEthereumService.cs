8d918495e2f60f335a9c7eb20aacb3f42fffd1d9
CasperCBroeren <c.broeren@gmail.com>
2017-03-29 17:50:16 +0200

    Update IEthereumService.cs
    Typo

--- src/EthereumStart/Services/IEthereumService.cs
+++ src/EthereumStart/Services/IEthereumService.cs
@@ -9,7 +9,7 @@ namespace EthereumStart.Services
         string AccountAddress { get; set; }
         Task<bool> SaveContractToTableStorage(EthereumContractInfo contract);
         Task<EthereumContractInfo> GetContractFromTableStorage(string name);
-        Task<decimal> GetBallance(string address);
+        Task<decimal> GetBalance(string address);
         Task<bool> ReleaseContract(string name, string abi, string byteCode, int gas);
         Task<string> TryGetContractAddress(string name);
         Task<Contract> GetContract(string name);
